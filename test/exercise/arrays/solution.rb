c7ff667d90eeef1f5fc4d5c024fcf3690d52947f
Bubnov Maxim <maksim.ungern@gmail.com>
2022-09-29 07:03:24 +0400

    solving tasks with arrays. the file .rubocop.yml has been edited in order to eliminate error 'Carriage return character detected'

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,31 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
+        max_elem = array[0]
+        array.each do |elem|
+          max_elem = elem if elem > max_elem
+        end
+        index = 0
+        array.each do |elem|
+          array[index] = max_elem if elem.positive?
+          index += 1
+        end
         array
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.length.zero?
+
+        mid_position = array.length / 2
+        return mid_position if array[mid_position] == query
+
+        if _array[mid_position] < query
+          recursive_res = search(array[mid_position + 1..array.length - 1], query)
+          return -1 if recursive_res == -1
+
+          return mid_position + 1 + recursive_res
+        end
+        search(array[0..mid_position - 1], query)
       end
     end
   end
