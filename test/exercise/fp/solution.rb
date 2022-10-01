a7d44db4dd179efb6eacc297e50ba2511f171742
Bubnov Maxim <maksim.ungern@gmail.com>
2022-10-01 08:29:05 +0400

    <fb> tasks solving

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,13 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        rating_array = array.map { |film| film['country'].to_s.split(',').length > 1 ? film['rating_kinopoisk'].to_f : 0 }
+        rating_array.reduce(0, :+) / rating_array.map { |rate| rate.positive? ? 1 : 0 }.reduce(0, :+)
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.map { |film| film['rating_kinopoisk'].to_f >= threshold ? film['name'].count('и') : 0 }.reduce(0, :+)
       end
     end
   end
